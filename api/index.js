6f9c617f6f93a712524eec031a09cb60d6f6804f
Chetan Naik <chetannaik11145@gmail.com>
2025-03-24 20:40:44 +0530

    latest-api

--- api/index.js
+++ api/index.js
@@ -1,30 +1,17 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Holdingsroutes = require("../Routes/Holdings");
-const Positionsroutes = require("../Routes/Positions");
-const OrderRoute = require("../Routes/Orders");
-require("dotenv").config();
-const MONGO_URL = process.env.MONGO_URL;
-const port = 8080;
+const Holdingsroutes = require("./Routes/Holdings");
+const Positionsroutes = require("./Routes/Positions");
+const OrderRoute = require("./Routes/Orders");
 const cookieParser = require("cookie-parser");
-const AuthRouter = require("../Routes/AuthRouter");
+require("dotenv").config();
+
+const app = express();
+
+// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-
-mongoose
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB is  connected successfully"))
-  .catch((err) => console.error(err));
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
@@ -33,13 +20,22 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 app.use(cookieParser());
 
-app.get("/", (req,res) => {
-  res.send("server is working fine");
-})
+// routes
 app.use("/", Holdingsroutes);
 app.use("/", Positionsroutes);
-app.use("/",  OrderRoute);
-app.use('/', AuthRouter);
+app.use("/", OrderRoute);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// export as vercel serverless handler
+const serverless = require("serverless-http");
+module.exports = serverless(app);
